34e4fa6b34f957be7f505bb6a18c0eb25b3393e8
AndreaDonato <donato.1808606@studenti.uniroma1.it>
2025-06-21 11:32:10 +0200

    Automatic Commit

---
+++ lab2/ex3/lrestart.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+kathara lclean
+xrdb ../../.XDefaults
+sudo kathara lstart --privileged
+for p in pc1 pc2 
+do
+  xterm -e bash -c "kathara connect $p" &
+done
+xrdb ../../.XDefaults.alt
+xterm -e bash -c "kathara connect r1" &
+
